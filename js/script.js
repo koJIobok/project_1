69e83f48d78929113705c42eeaf7e66592e22ca0
Vladimir <21203.vn@gmail.com>
2023-10-01 21:43:57 +0300

    js mod

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let firstUserAnswer = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: `${firstUserAnswer}`,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+
+console.log(personalMovieDB);
