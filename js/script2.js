8807909a4c28017e9fcdd1359fd7e3acca61e927
Vladimir <21203.vn@gmail.com>
2023-10-04 22:51:31 +0300

    practice

---
+++ js/script2.js
@@ -0,0 +1,70 @@
+"use strict";
+
+let str = 'texT';
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf('ruit'));
+
+const logg = 'Hello world!';
+
+// console.log(logg.slice(6));
+
+console.log(logg.substr(0, 5));
+
+const num = 19.5;
+
+console.log(Math.round(num));
+
+const test = '12.2px';
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
+console.log(test);
+
+
+// ------------------------------------------------------------------------------
+
+// function showFirstMessage() {
+//     console.log('Hello world!');
+// }
+
+// showFirstMessage();
+
+// const calc = (a , b) => { return a + b; };
+
+// console.log(calc(8, 29));
+
+// if (1) {
+//     console.log("It works!");
+// } else {
+//     console.log("Error");
+// }
+
+// ------------------------------------------------------------------------------
+
+// const num = 80;
+
+// // (num === '50') ? console.log("It works!") : console.log("Error");
+
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('В точку!');
+//         break;
+
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
